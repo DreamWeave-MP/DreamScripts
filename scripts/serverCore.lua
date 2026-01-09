3d7c7e04c63f5ef79160722ab2a461469390d77f
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-09 05:45:54 -0600

    FEAT: Embed customMerchantRestock directly

--- scripts/serverCore.lua
+++ scripts/serverCore.lua
@@ -30,6 +30,7 @@ animHelper = require("animHelper")
 speechHelper = require("speechHelper")
 menuHelper = require("menuHelper")
 require("defaultCommands")
+local customMerchantRestock = require 'customMerchantRestock'
 require("customScripts")
 
 Database = nil
