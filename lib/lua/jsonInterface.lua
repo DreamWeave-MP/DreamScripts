da5adf3955c03fc0fc3dd07ba11add75fd2a2a20
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-09 04:00:09 -0600

    CHECK: Don't try string conversion

--- lib/lua/jsonInterface.lua
+++ lib/lua/jsonInterface.lua
@@ -185,7 +185,7 @@ else
         local result = ffi.C.mkdir(path, DEFAULT_PERMS)
         print('mkdir result was: ' .. result)
         if result ~= 0 then
-            error('mkdir failed with error code: ' .. tostring(os.execute('echo $?')))
+            error('mkdir failed with error code: ' .. os.execute('echo $?'))
         end
         return result == 0
     end
