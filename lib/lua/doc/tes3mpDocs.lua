aad55085d62eff72175098407c47914ca2e423b8
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-09 02:40:13 -0600

    FEAT: Document GetOperatingSystemType

--- lib/lua/doc/tes3mpDocs.lua
+++ lib/lua/doc/tes3mpDocs.lua
@@ -1,6 +1,13 @@
 ---@meta
 
+---@alias OSType
+---| 'Windows'
+---| 'Linux'
+---| 'OS X'
+---| 'Unknown OS' -- This one probably shouldn't happen!
+
 ---@class TES3MPModule
 ---@field LogMessage fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
 ---@field LogAppend fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
+---@field GetOperatingSystemType fun(): OSType
 tes3mp = tes3mp
