289cae118c651d3a9650cd05365e9389490821c8
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-09 04:42:04 -0600

    CLEANUP: I swear...

--- lib/lua/jsonInterface.lua
+++ lib/lua/jsonInterface.lua
@@ -169,9 +169,10 @@ else
             return false
         end
 
-        local result = os.execute(('test -d %s'):format(path))
-        print('isDir result for ' .. path .. ' is: ' .. tostring(result) .. ' result type: ' .. type(result))
-        return result == F_OK
+        return os.execute(('test -d %s'):format(path))
+        -- local result = os.execute(('test -d %s'):format(path))
+        -- print('isDir result for ' .. path .. ' is: ' .. tostring(result) .. ' result type: ' .. type(result))
+        -- return result == F_OK
     end
 
     --- Given a path to a directory, attempt to create it.
