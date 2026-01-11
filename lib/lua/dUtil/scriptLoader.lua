6789356a9c32d0605e8394ac617953a5e0a5d877
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-11 02:18:46 -0600

    CHECK: Try printing key/interface

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -54,6 +54,7 @@ local Interfaces = {}
 DScriptLoader.Interfaces = setmetatable({},
   {
     __index = function(key)
+      print('searching for', Interfaces[key])
       return Interfaces[key]
     end,
     __newindex = function()
