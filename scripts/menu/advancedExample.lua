9690602e0964233db1fb23630454dde982a66eef
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-10 18:44:16 -0600

    CLEANUP: Run formatter on scripts/menu/advancedExample.lua

--- scripts/menu/advancedExample.lua
+++ scripts/menu/advancedExample.lua
@@ -3,19 +3,23 @@ Menus["advanced example origin"] = {
         color.Orange .. "Welcome, " .. color.Yellow,
         menuHelper.variables.currentPlayerVariable("data.login.name"),
         color.Orange .. "! This is an example of an advanced menu. Use it as a starting point for your own.\n\n" ..
-            color.White .. "Select what kind of functions you want to run."
+        color.White .. "Select what kind of functions you want to run."
     },
     buttons = {
-        { caption = "Player functions",
+        {
+            caption = "Player functions",
             destinations = { menuHelper.destinations.setDefault("advanced example player") }
         },
-        { caption = "World instance functions",
+        {
+            caption = "World instance functions",
             destinations = { menuHelper.destinations.setDefault("advanced example world") }
         },
-        { caption = "logicHandler functions",
+        {
+            caption = "logicHandler functions",
             destinations = { menuHelper.destinations.setDefault("advanced example logichandler") }
         },
-        { caption = "Global functions",
+        {
+            caption = "Global functions",
             destinations = { menuHelper.destinations.setDefault("advanced example global") }
         },
         { caption = "Exit", destinations = nil }
@@ -25,20 +29,22 @@ Menus["advanced example origin"] = {
 Menus["advanced example player"] = {
     text = color.Orange .. "Select a function to run on this player.",
     buttons = {
-        { caption = "Save()",
+        {
+            caption = "Save()",
             destinations = {
                 menuHelper.destinations.setDefault(nil,
-                {
-                    menuHelper.effects.runPlayerFunction("Save")
-                })
+                    {
+                        menuHelper.effects.runPlayerFunction("Save")
+                    })
             }
         },
-        { caption = "Message(\"This is a test\n\")",
+        {
+            caption = "Message(\"This is a test\n\")",
             destinations = {
                 menuHelper.destinations.setDefault(nil,
-                {
-                    menuHelper.effects.runPlayerFunction("Message", {"This is a test\n"})
-                })
+                    {
+                        menuHelper.effects.runPlayerFunction("Message", { "This is a test\n" })
+                    })
             }
         },
         { caption = "Back", destinations = { menuHelper.destinations.setFromCustomVariable("previousCustomMenu") } },
@@ -57,14 +63,15 @@ Menus["advanced example world"] = {
         color.Orange .. ". Select a function to run on this world."
     },
     buttons = {
-        { caption = "IncrementDay() and LoadTime(nil, true)",
+        {
+            caption = "IncrementDay() and LoadTime(nil, true)",
             destinations = {
                 menuHelper.destinations.setDefault(nil,
-                {
-                    menuHelper.effects.runGlobalFunction("WorldInstance", "IncrementDay"),
-                    menuHelper.effects.runGlobalFunction("WorldInstance", "LoadTime",
-                        {menuHelper.variables.currentPid(), true})
-                })
+                    {
+                        menuHelper.effects.runGlobalFunction("WorldInstance", "IncrementDay"),
+                        menuHelper.effects.runGlobalFunction("WorldInstance", "LoadTime",
+                            { menuHelper.variables.currentPid(), true })
+                    })
             }
         },
         { caption = "Back", destinations = { menuHelper.destinations.setFromCustomVariable("previousCustomMenu") } },
@@ -75,13 +82,14 @@ Menus["advanced example world"] = {
 Menus["advanced example logichandler"] = {
     text = color.Orange .. "Select a function to run on the logicHandler.",
     buttons = {
-        { caption = "CreateObjectAtPlayer(menuHelper.variables.currentPid(), \"rat\", \"spawn\")",
+        {
+            caption = "CreateObjectAtPlayer(menuHelper.variables.currentPid(), \"rat\", \"spawn\")",
             destinations = {
                 menuHelper.destinations.setDefault(nil,
-                {
-                    menuHelper.effects.runGlobalFunction("logicHandler", "CreateObjectAtPlayer",
-                        {menuHelper.variables.currentPid(), "rat", "spawn"})
-                })
+                    {
+                        menuHelper.effects.runGlobalFunction("logicHandler", "CreateObjectAtPlayer",
+                            { menuHelper.variables.currentPid(), "rat", "spawn" })
+                    })
             }
         },
         { caption = "Back", destinations = { menuHelper.destinations.setFromCustomVariable("previousCustomMenu") } },
@@ -92,13 +100,14 @@ Menus["advanced example logichandler"] = {
 Menus["advanced example global"] = {
     text = color.Orange .. "Select a global function to run.",
     buttons = {
-        { caption = "OnPlayerSendMessage(menuHelper.variables.currentPid(), \"This is a test chat message\")",
+        {
+            caption = "OnPlayerSendMessage(menuHelper.variables.currentPid(), \"This is a test chat message\")",
             destinations = {
                 menuHelper.destinations.setDefault(nil,
-                {
-                    menuHelper.effects.runGlobalFunction(nil, "OnPlayerSendMessage",
-                        {menuHelper.variables.currentPid(), "This is a test chat message"})
-                })
+                    {
+                        menuHelper.effects.runGlobalFunction(nil, "OnPlayerSendMessage",
+                            { menuHelper.variables.currentPid(), "This is a test chat message" })
+                    })
             }
         },
         { caption = "Back", destinations = { menuHelper.destinations.setFromCustomVariable("previousCustomMenu") } },
