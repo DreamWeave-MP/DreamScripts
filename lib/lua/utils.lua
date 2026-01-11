9ddf3961c547189091fa9c81e94eb3449f27de2c
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-10 19:19:06 -0600

    CLEANUP: Refactor string meta overrides

--- lib/lua/utils.lua
+++ lib/lua/utils.lua
@@ -1,28 +1,28 @@
-function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) fields[#fields+1] = c end)
+local StringMeta = getmetatable('').__index
+
+function StringMeta:trim()
+    return (self:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+function StringMeta:split(sep)
+    sep = sep or ':'
+    local fields, pattern = {}, ('([^%s]+)'):format(sep)
+    self:gsub(pattern, function(c) fields[#fields + 1] = c end)
     return fields
 end
 
-function string:capitalizeFirstLetter()
+function StringMeta:capitalizeFirstLetter()
     return (self:gsub("^%l", string.upper))
 end
 
 -- Check for case-insensitive equality
-function string:ciEqual(otherString)
-
+function StringMeta:ciEqual(otherString)
     if type(otherString) ~= "string" then return false end
 
     return self:lower() == otherString:lower()
 end
 
-function string:trim()
-    return (self:gsub("^%s*(.-)%s*$", "%1"))
-end
-
 function prefixZeroes(inputString, desiredLength)
-
     local length = string.len(inputString)
 
     while length < desiredLength do
