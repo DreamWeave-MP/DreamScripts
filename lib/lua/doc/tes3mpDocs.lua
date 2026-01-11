5c4e407e2938db9c16035bec35d5fc40e5df65b1
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-10 18:18:16 -0600

    CLEANUP: Rearrange by alphabet

--- lib/lua/doc/tes3mpDocs.lua
+++ lib/lua/doc/tes3mpDocs.lua
@@ -7,7 +7,7 @@
 ---| 'Unknown OS' -- This one probably shouldn't happen!
 
 ---@class TES3MPModule
+---@field GetOperatingSystemType fun(): OSType
 ---@field LogMessage fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
 ---@field LogAppend fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
----@field GetOperatingSystemType fun(): OSType
 tes3mp = tes3mp
