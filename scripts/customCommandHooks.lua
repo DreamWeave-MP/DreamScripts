7d099e34fa59bf1980613ac1d0f173a2d3c36472
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-12 01:38:10 -0600

    FIX: Correct import error again

--- scripts/customCommandHooks.lua
+++ scripts/customCommandHooks.lua
@@ -20,7 +20,7 @@
 ]]
 
 local enumerations = require 'tes3mp.enumerations'
-local tableHelper = require 'tesmp3.util.table'
+local tableHelper = require 'tes3mp.util.table'
 
 ---@class CustomCommandHooks
 ---@field commands table<string, TES3MPCommand>
