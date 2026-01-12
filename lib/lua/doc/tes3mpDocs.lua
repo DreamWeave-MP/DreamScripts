87ea4f2c882fad8d54b8896a659c02a4c6d8d167
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-11 20:38:35 -0600

    FEAT: Annotate some more GUI functions in tes3mp

--- lib/lua/doc/tes3mpDocs.lua
+++ lib/lua/doc/tes3mpDocs.lua
@@ -2,6 +2,7 @@
 
 ---@alias CommandTokens string[] pre-tokenized command string provided to all commandHandlers. The first value is always the name of the command entered, including the leading slash.
 ---@alias CommandHandler fun(pid: PlayerId, cmd: CommandTokens) Special function type for handling chat commands
+---@alias GUIID integer Unique numeric Identifier for GUIs
 ---@alias Inventory Item[]
 ---@alias PlayerId integer zero-based integer indicating a unique player ID.
 
@@ -31,8 +32,10 @@
 ---@field GetLastPlayerId fun(): PlayerId returns the last PID which connected to the server
 ---@field GetOperatingSystemType fun(): OSType
 ---@field GetSHA256Hash fun(input: string): string Given some string input, hashes it
+---@field InputDialog fun(pid: PlayerId, id: GUIID, label: string, note: string) Displays an input dialog
 ---@field LogMessage fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
 ---@field LogAppend fun(logLevel: LogLevel, logMessage: string) Emits a message to the server log & stdout at the provided log level
+---@field PasswordDialog fun(pid: PlayerId, id: GUIID, label: string, note: string) Displays an input dialog whose inputs are displayed only as asterisks
 ---@field SendMessage fun(pid: PlayerId, message: string, sendToAll: boolean?) Emits a chat message to a specific player, optionally relaying it to all players
 ---@field StopServer fun(exitCode: integer) Terminate the server with a provided exit code
 ---@field UnbanAddress fun(ipAddress: string) Given an IP Address string, unbans it. Doesn't perform any validation, so caller functions need to do so themselves.
