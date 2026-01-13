685861a3397e33f3caf951bcbc843963a1e3fc02
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-12 22:40:29 -0600

    CHECK: Broke more shit

--- scripts/serverCore.lua
+++ scripts/serverCore.lua
@@ -341,6 +341,8 @@ end
 
 function OnServerScriptCrash(errorMessage)
     tes3mp.LogMessage(enumerations.log.ERROR, "Server crash from script error!")
+    tes3mp.StopServer(7)
+    if not ScriptLoader.Interfaces.customEventHooks then return end
     ScriptLoader.Interfaces.customEventHooks.triggerHandlers("OnServerExit",
         ScriptLoader.Interfaces.customEventHooks.makeEventStatus(true, true), { errorMessage })
 end
