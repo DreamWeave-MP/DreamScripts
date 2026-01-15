0eeaecdffa7265e0720dec70e6494cc7e4f16fbe
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-15 16:08:53 -0600

    FIX: Correct missing directory suffixes

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -101,7 +101,7 @@ function DScriptLoader.requireShim(scriptName)
     print('Loading fresh script ' .. scriptName)
     local ok, chunk, err = false, nil, nil
 
-    for _, prefix in ipairs { 'server/scripts', 'lib/', 'lib/lua', } do
+    for _, prefix in ipairs { 'server/scripts/', 'lib/', 'lib/lua/', } do
       local checkPath = ('%s%s.lua'):format(prefix, scriptName:gsub('%.', PathSeparator))
 
       chunk, err = loadfile(checkPath)
