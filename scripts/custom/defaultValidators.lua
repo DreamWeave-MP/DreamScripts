2eab4b4fe28d089c7e3f30155465c8692829e705
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-15 17:15:51 -0600

    CHECK: Is it the player themselves that's undefined?

--- scripts/custom/defaultValidators.lua
+++ scripts/custom/defaultValidators.lua
@@ -32,7 +32,7 @@ return {
     -- Don't allow console commands from players who lack the permissions for them and haven't been asked
     -- to run the console commands by the server itself; kick them instead
     OnConsoleCommand = function(_, pid, cellDescription, consoleCommand, _, _)
-      print('Players table is: ', Players or 'FUCKED')
+      print('Players table is: ', Players or 'FUCKED', 'Player is: ', Players[pid])
       local hasConsoleCommandQueued = tableHelper.containsValue(Players[pid].consoleCommandsQueued, consoleCommand)
 
       if not logicHandler.IsPlayerAllowedConsole(pid) and not hasConsoleCommandQueued then
