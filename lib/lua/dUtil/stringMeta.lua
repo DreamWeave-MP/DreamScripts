805de45212749f5a4e070960b1e64a3ad8d8872c
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-14 19:38:14 -0600

    CLEANUP: Fix string override annotations

--- lib/lua/dUtil/stringMeta.lua
+++ lib/lua/dUtil/stringMeta.lua
@@ -1,3 +1,9 @@
+---@class stringlib
+---@field split fun(self: string, separator: string): string[] Splits a string by a separator and returns an array of matching substrings
+---@field trim fun(self: string): string Removes whitespace from the beginning and end of a string
+---@field capitalizeFirstLetter fun(self: string): string
+---@field ciEqual fun(self: string, otherString: string): boolean case-insensitive equality comparison
+
 local StringMeta = getmetatable('').__index
 
 function StringMeta:trim()
@@ -15,7 +21,6 @@ function StringMeta:capitalizeFirstLetter()
   return (self:gsub("^%l", string.upper))
 end
 
--- Check for case-insensitive equality
 function StringMeta:ciEqual(otherString)
   if type(otherString) ~= "string" then return false end
 
