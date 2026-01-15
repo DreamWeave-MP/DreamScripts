a67e2fcd72f7762f592e915347a18a91039dead0
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-15 12:19:35 -0600

    FIX: Change hidden debug module to be named scriptLoader instead, since `debug` is a real thing that actually exists (but that we don't give you access to :P)

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -102,7 +102,8 @@ function DScriptLoader.getScriptEnv()
     string = string,
     table = table,
     tes3mp = tes3mp,
-    debug = {
+    ---@class DScriptLoaderHidden
+    scriptLoader = {
       loadScript = DScriptLoader.loadScript,
       loadAllScripts = DScriptLoader.loadAllScripts,
     },
