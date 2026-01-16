8858eabc5a7819b6977c17824864f0a3f3d7c1e5
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 02:56:15 -0600

    FIX: Log another way??

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -27,6 +27,6 @@ for _, pluginName in ipairs(loadOrder) do
 
   for i = 1, #plugin.objects do
     local object = plugin.objects[i]
-    tes3mp.LogAppend(enumerations.log.WARN, object.id .. ' ' .. object.type)
+    tes3mp.LogAppend(enumerations.log.WARN, tostring(object))
   end
 end
