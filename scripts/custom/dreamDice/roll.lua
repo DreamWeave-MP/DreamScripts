a9bcf744765ebb62bf6dbbb566b072dd4ca0cc2d
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-15 18:22:24 -0600

    CHECK: Where are the colors?

--- scripts/custom/dreamDice/roll.lua
+++ scripts/custom/dreamDice/roll.lua
@@ -3,7 +3,9 @@
 ---@alias RollNum integer positive number of dice to roll. Minimum of 1, but any positive bounds
 ---@alias RollFaces integer positive number of faces to roll for each die. Minimum of 1, but any positive bounds
 
+local tableHelper = require 'tes3mp.util.table'
 local color = require 'color'
+tableHelper.print(color or {})
 
 math.randomseed(os.time())
 
