d78ce61e509046c601083070c25c1b4d9ee70f39
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-15 20:14:39 -0600

    FEAT: Better dynamic generation of dreamDice tests and logging

--- scripts/custom/dreamDice/test_rolls.lua
+++ scripts/custom/dreamDice/test_rolls.lua
@@ -1,20 +1,43 @@
+local enumerations = require 'tes3mp.enumerations'
+
 local I = require 'interfaces'
 
-local testPairs = {
-  [3] = 4,
-  [1] = 7,
-  [2] = 3,
-  [5] = 6,
-  [4] = 8,
-  [7] = 6,
-}
-local DefaultTestIterations = 1000
+local testPairs = {}
 
+local maxElements = math.random(100) * math.random(100)
+local usedKeys, numKeys = {}, 0
+
+for _ = 1, maxElements do
+  local randomKey
+
+  repeat
+    randomKey = math.random(1, maxElements + 1)
+  until not usedKeys[randomKey] -- Check if key already used
+
+  usedKeys[randomKey] = true
+
+  testPairs[randomKey] = math.random(20)
+  numKeys = numKeys + 1
+end
+
+tes3mp.LogAppend(
+  enumerations.log.INFO,
+  ('Generating %d random dice pairs for dreamDice integration tests . . .'):format(numKeys)
+)
+
+usedKeys = nil
+
+local DefaultTestIterations = 1000
 ---@param testIterations integer?
 return function(testIterations)
   if not testIterations or type(testIterations) ~= 'number' then
     testIterations = DefaultTestIterations
   end
+
+  tes3mp.LogAppend(
+    enumerations.log.INFO,
+    ('DreamDice integration tests running for %d dice, with %d iterations each.'):format(numKeys, testIterations)
+  )
 
   for dice, faces in pairs(testPairs) do
     for _ = 1, testIterations do
