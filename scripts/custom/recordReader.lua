92aa47d13011bb060ee5bd62d59a63ebf9f3555d
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 03:00:14 -0600

    CLEANUP: Only log object id

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -33,7 +33,8 @@ for _, pluginName in ipairs(loadOrder) do
   for i = 1, #plugin.objects do
     local object = plugin.objects[i]
     if object.type == 'Static' then
-      tes3mp.LogAppend(enumerations.log.WARN, tostring(object))
+      tes3mp.LogAppend(enumerations.log.WARN, tostring(object.id))
+      -- tes3mp.LogAppend(enumerations.log.WARN, tostring(object))
     end
     -- tes3mp.LogAppend(enumerations.log.WARN, tostring(object))
   end
