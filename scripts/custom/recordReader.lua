edc2e0730b00e548c7e770fbbc59d905d578f424
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 02:48:27 -0600

    CHECK: Is it the loop?

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -25,7 +25,7 @@ for _, pluginName in ipairs(loadOrder) do
   local plugin = tes3.load_plugin(pluginPath)
   tes3mp.LogAppend(enumerations.log.WARN, 'Records defined by ' .. pluginPath)
 
-  for i = 1, #plugin.objects do
-    local object = plugin.objects[i]
-  end
+  -- for i = 1, #plugin.objects do
+  --   local object = plugin.objects[i]
+  -- end
 end
