1fcbd5513460932bd5d97dc2c33ab171d6524bf8
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-17 01:25:14 -0600

    CHECK: Print the result table to make sure it's hash

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -430,10 +430,6 @@ return {
   },
   eventValidators = {
     OnServerPostInit = function()
-      dUtil.table(dUtil.tableType.HASH, nil, {
-        fuck = 'you'
-      })
-
       local startClock = os.clock()
 
       local logStr = ('Successfully loaded %d records in %.3f seconds.\n')
@@ -451,6 +447,10 @@ return {
           break
         end
       end
+
+      print(dUtil.table(dUtil.tableType.HASH, nil, {
+        fuck = 'you'
+      }))
     end
   }
 }
