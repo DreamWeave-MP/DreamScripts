7a8ce68c67ec652d54f171bd43ea8f60a69378f5
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 20:01:13 -0600

    CLEANUP: Don't seppuku if tes3 is not defined

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -51,7 +51,6 @@ local saveDataTable = BufferedDiskPaths
 function DScriptLoader.originalInterfaces()
   local hasTDS, tds = pcall(require, 'tds.init')
   local hasTES3, tes3 = pcall(require, 'tes3_lua')
-  if not hasTES3 then error(tes3 .. '\n' .. debug.traceback()) end
 
   ---@type DefaultInterfaces
   return {
