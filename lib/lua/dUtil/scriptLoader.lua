782e667eb08d1c2c250680443d76aa5bfe3c5952
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 19:51:43 -0600

    CHECK: Does it need lib prefix?

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -50,7 +50,7 @@ local saveDataTable = BufferedDiskPaths
 ---@return DefaultInterfaces
 function DScriptLoader.originalInterfaces()
   local hasTDS, tds = pcall(require, 'tds.init')
-  local hasTES3, tes3 = pcall(require, 'tes3_lua')
+  local hasTES3, tes3 = pcall(require, 'libtes3_lua')
   if not hasTES3 then error(debug.traceback()) end
 
   ---@type DefaultInterfaces
