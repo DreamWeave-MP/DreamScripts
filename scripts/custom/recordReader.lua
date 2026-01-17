e3d4280715fe1f12b74aed0e4e27d9a19fde6510
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-16 18:20:43 -0600

    CHECK: Where is all the data tho

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -243,6 +243,10 @@ return {
         ('Successfully loaded %d records in %.3f seconds.'):format(createRecordStores(), os.clock() - startClock)
       )
 
+      for k, v in pairs(I.recordStores) do
+        print(k, v)
+      end
+
       I.recordStores.Static.someRecord = 'value'
 
       for storeType, recordStore in pairs(RecordStores) do
