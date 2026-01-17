81a544389557f183438f15def93f747a0257e913
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-17 16:00:41 -0600

    Revert "CLEANUP: When possible make the cache a tds.Hash"
    Actually that was a stupid idea because modules generally don't return
    anything that fits in a tds hash anyway

--- lib/lua/dUtil/scriptLoader.lua
+++ lib/lua/dUtil/scriptLoader.lua
@@ -164,7 +164,7 @@ DScriptLoader.Interfaces = setmetatable({},
 local bit, ffi = require 'bit', require 'ffi'
 local hasCJSON, cjson = pcall(require, 'cjson')
 function DScriptLoader.defaultModuleCache()
-  local cache = {
+  return {
     bit = bit,
     cjson = hasCJSON and cjson or nil,
     ['dutil.init'] = dUtil,
@@ -172,10 +172,6 @@ function DScriptLoader.defaultModuleCache()
     interfaces = DScriptLoader.Interfaces,
     jsoninterface = jsonInterface,
   }
-
-  if tds then cache = tds.Hash(cache) end
-
-  return cache
 end
 
 local PathSeparator = tes3mp.GetOperatingSystemType() == 'Windows' and '\\' or '/'
