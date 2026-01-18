dc7e02c659dde6aa960c74f8d952a989b73b48bc
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-18 00:14:18 -0600

    CHECK: Wtf

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -689,6 +689,7 @@ local TypeHandlers = {
         attributes:resize(numAttributes)
 
         for j, attribute in ipairs(requirement.attributes) do
+          print(attribute)
           attributes[j] = numberField(attribute)
         end
       end
