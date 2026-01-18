d045e96a5a0c362dc1ca7bef2ff80c84a82f9634
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-18 17:40:55 -0600

    CLEANUP: Do the assert differently

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -66,7 +66,7 @@ end
 
 ---@param trans number[] array with three numeric values
 local function transform(trans)
-  assert(#trans == 3, tostring(trans))
+  assert(trans and #trans == 3, tostring(trans))
   return vector(tonumber(trans[1]), tonumber(trans[2]), tonumber(trans[3]))
 end
 
