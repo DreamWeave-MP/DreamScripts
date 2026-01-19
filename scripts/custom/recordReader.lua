dfe2cfc7f87c05f135ccfbfab077e0f395048ef1
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-19 14:46:09 -0600

    FIX: Don't tolerate empty ids

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1456,7 +1456,9 @@ local function idIsFree(recordId, object)
   assert(recordType)
 
   -- We don't necessarily save everything, so skip object generation for those
-  if not RecordStores[recordType] then return false end
+  -- Also there are some dumb edge cases where records have ids defined
+  -- But they are empty, we're just gonna reject those because they're stupid
+  if recordId == '' or not RecordStores[recordType] then return false end
 
   --- Cells are an exception to this rule
   --- As they must merge, so the id is always considered 'free'
