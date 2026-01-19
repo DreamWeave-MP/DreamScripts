8c937c2a4a665e4d2b96cab2c40c6aa742fd21e3
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-19 01:10:23 -0600

    FIX: I guess we should be able to do this

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1403,7 +1403,7 @@ local function createRecordStores()
         --- Since we iterate in reverse, skip records in
         --- this store which have already been defined
         --- Technically this isn't good enough as we should do a placeable check as well
-        if not recordStore[recordId] then
+        if not recordId or not recordStore[recordId] then
           local resultRecord = typeHandler(object, recordId)
 
           local inputId = recordId or resultRecord.id
