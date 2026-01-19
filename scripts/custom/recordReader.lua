012a06322e2244710279fb77e5eb0bac387af932
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-18 22:49:38 -0600

    CLEANUP: Where did those come from?

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -969,8 +969,6 @@ local TypeHandlers = {
     hash.race = MandatoryRecordId(record.race)
     hash.rank = numberField(record.data.rank)
     hash.reputation = numberField(record.data.reputation)
-    hash.value = numberField(record.data.value)
-    hash.weight = numberField(record.data.weight)
 
     if record.data.stats and not bit.band(Enums.Flags.NPC.AUTO_CALCULATE, hash.npcFlags) then
       local stats = record.data.stats
