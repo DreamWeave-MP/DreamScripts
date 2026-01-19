d9e1081717b33fbf379a24cb29ff5c1f6928f89b
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-19 14:22:54 -0600

    CLEANUP: I forget why it works this way so add an assert to check for objects without ids

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1501,6 +1501,7 @@ local function createRecordStores()
       local recordStore, typeHandler = RecordStores[object.type], TypeHandlers[object.type]
 
       if recordStore and typeHandler then
+        assert(recordId, tostring(object) .. '\n' .. object.type)
         local resultRecord = typeHandler(object, recordId)
         recordId = recordId or resultRecord.id
 
