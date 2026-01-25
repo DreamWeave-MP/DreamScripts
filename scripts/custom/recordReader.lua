39ba4b14f837e504ad9a3cbc370a4fc73c926259
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-25 14:50:22 -0600

    Revert "CHECK: Try skipping tostring conversion on gmst values for numeric types"
    This reverts commit 3523eaf7cc14eed0ffe4f3156ba23751e8f32a5f.
    They need to be tostring'ed :(

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -950,7 +950,7 @@ local TypeHandlers = {
     if record.value.type == 'String' then
       return assert(tostring(record.value))
     else
-      return numberField(record.value)
+      return numberField(tostring(record.value))
     end
   end,
 
