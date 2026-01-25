ef46b95ed0b2d309b029ac137a368773fc191a6c
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-25 14:44:54 -0600

    FIX: Actually that should be nil instead of empty string

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1665,8 +1665,8 @@ local function createRecordStores()
     local lowerPluginName = pluginName:lower()
 
     for j, object in ipairs(tes3.load_plugin(pluginPath).objects) do
-      local recordId = object.id or ''
-      if recordId ~= '' then recordId = recordId:lower() end
+      local recordId = object.id
+      if recordId then recordId = recordId:lower() end
 
       if idIsFree(recordId, object) then
         local recordStore, typeHandler = RecordStores[object.type], TypeHandlers[object.type]
