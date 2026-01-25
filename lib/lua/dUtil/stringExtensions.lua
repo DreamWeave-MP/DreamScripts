3491fb40e867f8080d5c2f706ca68c33493eb069
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-25 14:59:51 -0600

    FIX: Forgot to save file

--- lib/lua/dUtil/stringExtensions.lua
+++ lib/lua/dUtil/stringExtensions.lua
@@ -65,15 +65,3 @@ function StringMeta:splitUniqueIndex()
   local splitIndex = self:split('-')
   return math.floor(assert(tonumber(splitIndex[1]))), math.floor(assert(tonumber(splitIndex[2])))
 end
-
-do
-  local hasResty, result = pcall(require, 'table.isempty')
-  if hasResty then
-    table.isempty = result
-    table.isarray = require 'table.isarray'
-    table.nkeys = require 'table.nkeys'
-    table.clone = require 'table.clone'
-  end
-
-  table.new = require 'table.new'
-end
