5b03f584a5da00acf06d195bf67f3c9cfe19ff98
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-26 17:39:57 -0600

    Revert "FIX: Call tonumber on numeric GMST values"
    This reverts commit ea5cd277f2a258982834020b8a9770988c3df268.

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1108,7 +1108,7 @@ local TypeHandlers = {
     if record.value.type == 'String' then
       return assert(tostring(record.value))
     else
-      return numberField(tonumber(record.value))
+      return numberField(tostring(record.value))
     end
   end,
 
