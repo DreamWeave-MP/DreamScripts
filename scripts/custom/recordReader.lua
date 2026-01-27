c1beb590f7ce871ac4e10eb476485212cdb8c1c7
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-26 21:39:55 -0600

    FIX: Lowercase plugin names when storing them in the LO

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -36,7 +36,7 @@ for i, loadOrderData in ipairs(RequiredDataFiles) do
     :format(pluginName)
   )
 
-  loadOrder[i] = pluginName
+  loadOrder[i] = lowerPluginName
 
   foundPlugins[lowerPluginName] = true
 end
