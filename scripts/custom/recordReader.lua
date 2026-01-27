e67d795dd1ab08c0afb3c690ba74f2c9e053eb04
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-01-26 21:43:54 -0600

    FIX: Don't tostring ingredient fields

--- scripts/custom/recordReader.lua
+++ scripts/custom/recordReader.lua
@@ -1170,22 +1170,22 @@ local TypeHandlers = {
     object.value = numberField(record.data.value)
     object.weight = numberField(record.data.weight)
     object.effects = {
-      numberField(Enums.MagicEffectId[tostring(record.data.effects[1])]),
-      numberField(Enums.MagicEffectId[tostring(record.data.effects[2])]),
-      numberField(Enums.MagicEffectId[tostring(record.data.effects[3])]),
-      numberField(Enums.MagicEffectId[tostring(record.data.effects[4])])
+      numberField(Enums.MagicEffectId[record.data.effects[1]]),
+      numberField(Enums.MagicEffectId[record.data.effects[2]]),
+      numberField(Enums.MagicEffectId[record.data.effects[3]]),
+      numberField(Enums.MagicEffectId[record.data.effects[4]])
     }
     object.skills = {
-      numberField(Enums.SkillId[tostring(record.data.skills[1])]),
-      numberField(Enums.SkillId[tostring(record.data.skills[2])]),
-      numberField(Enums.SkillId[tostring(record.data.skills[3])]),
-      numberField(Enums.SkillId[tostring(record.data.skills[4])])
+      numberField(Enums.SkillId[record.data.skills[1]]),
+      numberField(Enums.SkillId[record.data.skills[2]]),
+      numberField(Enums.SkillId[record.data.skills[3]]),
+      numberField(Enums.SkillId[record.data.skills[4]])
     }
     object.attributes = {
-      numberField(Enums.AttributeId[tostring(record.data.attributes[1])]),
-      numberField(Enums.AttributeId[tostring(record.data.attributes[2])]),
-      numberField(Enums.AttributeId[tostring(record.data.attributes[3])]),
-      numberField(Enums.AttributeId[tostring(record.data.attributes[4])])
+      numberField(Enums.AttributeId[record.data.attributes[1]]),
+      numberField(Enums.AttributeId[record.data.attributes[2]]),
+      numberField(Enums.AttributeId[record.data.attributes[3]]),
+      numberField(Enums.AttributeId[record.data.attributes[4]])
     }
 
     if isDeleted then object.isDeleted = true end
